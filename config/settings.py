f633fcd9c3c57da445008adf22495f8ed04b9f1e
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2022-12-08 09:17:08 +0330

    setup authentication

--- config/settings.py
+++ config/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
+    "rest_framework",
+    "rest_framework.authtoken",
+    "djoser",
 
-    "attendance.apps.AttendanceConfig"
+    "attendance.apps.AttendanceConfig",
 ]
 
 MIDDLEWARE = [
@@ -100,6 +104,12 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+}
 
 
 # Internationalization
