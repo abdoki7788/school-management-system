e5b073563c5231b8f1154a690b75472c10e280b5
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2022-12-08 16:05:46 +0330

    create project models base

--- attendance/models.py
+++ attendance/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Attendance(models.Model):
+    class_room = models.ForeignKey("Class", on_delete=models.CASCADE, related_name="attendances")
+    absents = models.ManyToManyField("Student", related_name="absent")
+    presents = models.ManyToManyField("Student", related_name="present")
+    date = models.DateField(default=datetime.now)
+
+class Student(models.Model):
+    first_name = models.CharField(verbose_name="First Name", max_length=50)
+    last_name = models.CharField(verbose_name="Last Name", max_length=50)
+    number = models.CharField(max_length=11, verbose_name="Phone Number")
+    class_room = models.ForeignKey("Class", verbose_name="Class", on_delete=models.SET_NULL, null=True, related_name="students")
+
+
+class Class(models.Model):
+    class_id = models.CharField(max_length=3)
