c2e550bdbfa92a3c396559fe2293d90d781304ba
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2022-12-08 18:52:32 +0330

    create serializer, view and router for classes

--- attendance/views.py
+++ attendance/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
+from .serializers import ClassSerializer
+from .models import Class
 
-# Create your views here.
+User = get_user_model()
+
+class ClassViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing user instances.
+    """
+    serializer_class = ClassSerializer
+    queryset = Class.objects.all()
