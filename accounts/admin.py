276e69056a1b126da60c1a9bc16481d817a66bd9
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2022-12-08 09:43:39 +0330

    create custom user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                    "type"
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login",)}),
+    )
+
+admin.site.register(User, CustomUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    TYPE_CHOICES = (
+        ("H", "Headmaster"),
+        ("S", "Staff"),
+        ("T", "Teacher"),
+    )
+    type = models.CharField(choices=TYPE_CHOICES, max_length=1)
