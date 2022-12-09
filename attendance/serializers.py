983b34078a6d0a44b2eaa4689f16e1f41514075f
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2022-12-09 16:48:07 +0330

    create studentSerializer

--- attendance/serializers.py
+++ attendance/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Class
+from .models import Class, Student
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ["id", "first_name", "last_name", "number"]
 
 class ClassSerializer(serializers.ModelSerializer):
+    students = StudentSerializer(many=True)
     class Meta:
         model = Class
         fields = ["id", "class_id", "students"]
