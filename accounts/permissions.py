61d90f7b88748744314d19c36a54acf13b44d4b8
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2023-05-23 07:54:45 +0430

    fix permissions bug

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -3,9 +3,11 @@ from rest_framework import permissions
 class IsHeadmaster(permissions.BasePermission):
     ## if user is headmaster , then can access to view
     def has_permission(self, request, view):
+        if not request.user.is_authenticated: return False
         return request.user.type == 'H'
 
 class IsHeadmasterOrReadonly(permissions.BasePermission):
     # if user is headmaster has full access to view and else can just see content
     def has_permission(self, request, view):
+        if not request.user.is_authenticated: return False
         return request.user.type == 'H' or (request.user.type in ('S', 'T') and request.method in permissions.SAFE_METHODS)
--- attendance/permissions.py
+++ attendance/permissions.py
@@ -3,4 +3,5 @@ from rest_framework import permissions
 class IsSchoolStaffOrReadOnly(permissions.BasePermission):
     # if user is school staff has full access to view and else can just see content
     def has_permission(self, request, view):
+        if not request.user.is_authenticated: return False
         return (request.user.type in ('H', 'S') or request.method in permissions.SAFE_METHODS)
